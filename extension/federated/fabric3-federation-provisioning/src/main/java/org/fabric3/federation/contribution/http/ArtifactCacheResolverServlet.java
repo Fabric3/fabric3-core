bb87d130f82760162c8182750b2680cfdd740d91
jimmarino <jim.marino@gmail.com>
2010-01-29 00:29:04 +0000

    copy module

---
+++ extension/federated/fabric3-federation-provisioning/src/main/java/org/fabric3/federation/contribution/http/ArtifactCacheResolverServlet.java
@@ -0,0 +1,88 @@
+/*
+* Fabric3
+* Copyright (c) 2009 Metaform Systems
+*
+* Fabric3 is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as
+* published by the Free Software Foundation, either version 3 of
+* the License, or (at your option) any later version, with the
+* following exception:
+*
+* Linking this software statically or dynamically with other
+* modules is making a combined work based on this software.
+* Thus, the terms and conditions of the GNU General Public
+* License cover the whole combination.
+*
+* As a special exception, the copyright holders of this software
+* give you permission to link this software with independent
+* modules to produce an executable, regardless of the license
+* terms of these independent modules, and to copy and distribute
+* the resulting executable under terms of your choice, provided
+* that you also meet, for each linked independent module, the
+* terms and conditions of the license of that module. An
+* independent module is a module which is not derived from or
+* based on this software. If you modify this software, you may
+* extend this exception to your version of the software, but
+* you are not obligated to do so. If you do not wish to do so,
+* delete this exception statement from your version.
+*
+* Fabric3 is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty
+* of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+* See the GNU General Public License for more details.
+*
+* You should have received a copy of the
+* GNU General Public License along with Fabric3.
+* If not, see <http://www.gnu.org/licenses/>.
+*/
+package org.fabric3.federation.contribution.http;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.net.URL;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.fabric3.spi.artifact.ArtifactCache;
+import org.fabric3.util.io.IOHelper;
+
+/**
+ * Used on a participant runtime to return the contents of a contribution associated with the encoded servlet path from the local artifact cache. The
+ * servlet path corresponds to the contribution URI.
+ *
+ * @version $Rev: 7888 $ $Date: 2009-11-22 11:27:32 +0100 (Sun, 22 Nov 2009) $
+ */
+public class ArtifactCacheResolverServlet extends HttpServlet {
+    private static final long serialVersionUID = 7721634599080335126L;
+    private ArtifactCache cache;
+
+    public ArtifactCacheResolverServlet(ArtifactCache cache) {
+        this.cache = cache;
+    }
+
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+        String pathInfo = req.getPathInfo();
+        if (pathInfo == null || pathInfo.length() < 2) {
+            resp.sendError(HttpServletResponse.SC_FORBIDDEN);
+            return;
+        }
+
+        String info = req.getPathInfo().substring(1);    // path info always begins with '/'
+        try {
+            URI uri = new URI(info);
+            URL url = cache.get(uri);
+            if (url == null) {
+                throw new ServletException("Contribution not found: " + info + ". Request URL was: " + info);
+            }
+            IOHelper.copy(url.openStream(), resp.getOutputStream());
+        } catch (URISyntaxException e) {
+            throw new ServletException("Invalid URI: " + info, e);
+        }
+    }
+
+
+}
---
+++ extension/federated/fabric3-federation-provisioning/src/main/java/org/fabric3/federation/contribution/http/ZoneContributionResolverExtension.java
@@ -0,0 +1,95 @@
+/*
+* Fabric3
+* Copyright (c) 2009 Metaform Systems
+*
+* Fabric3 is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as
+* published by the Free Software Foundation, either version 3 of
+* the License, or (at your option) any later version, with the
+* following exception:
+*
+* Linking this software statically or dynamically with other
+* modules is making a combined work based on this software.
+* Thus, the terms and conditions of the GNU General Public
+* License cover the whole combination.
+*
+* As a special exception, the copyright holders of this software
+* give you permission to link this software with independent
+* modules to produce an executable, regardless of the license
+* terms of these independent modules, and to copy and distribute
+* the resulting executable under terms of your choice, provided
+* that you also meet, for each linked independent module, the
+* terms and conditions of the license of that module. An
+* independent module is a module which is not derived from or
+* based on this software. If you modify this software, you may
+* extend this exception to your version of the software, but
+* you are not obligated to do so. If you do not wish to do so,
+* delete this exception statement from your version.
+*
+* Fabric3 is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty
+* of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+* See the GNU General Public License for more details.
+*
+* You should have received a copy of the
+* GNU General Public License along with Fabric3.
+* If not, see <http://www.gnu.org/licenses/>.
+*/
+package org.fabric3.federation.contribution.http;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URI;
+
+import org.osoa.sca.annotations.EagerInit;
+import org.osoa.sca.annotations.Property;
+import org.osoa.sca.annotations.Reference;
+
+import org.fabric3.spi.contribution.ContributionResolverExtension;
+import org.fabric3.spi.contribution.ResolutionException;
+import org.fabric3.spi.topology.MessageException;
+import org.fabric3.spi.topology.Response;
+import org.fabric3.spi.topology.ZoneTopologyService;
+
+/**
+ * Resolves contributions in a domain. Resolution is done by first querying a zone leader for the contribution URL. If the current runtime is the zone
+ * leader, the controller is queried for the contribution URL.
+ *
+ * @version $Rev$ $Date$
+ */
+@EagerInit
+public class ZoneContributionResolverExtension implements ContributionResolverExtension {
+    private ZoneTopologyService topologyService;
+    private long defaultTimeout = 10000;
+
+    public ZoneContributionResolverExtension(@Reference ZoneTopologyService topologyService) {
+        this.topologyService = topologyService;
+    }
+
+    @Property(required = false)
+    public void setDefaultTimeout(long defaultTimeout) {
+        this.defaultTimeout = defaultTimeout;
+    }
+
+    public InputStream resolve(URI contributionUri) throws ResolutionException {
+        String zoneLeader = topologyService.getZoneLeader();
+        ProvisionCommand command = new ProvisionCommand(contributionUri);
+        try {
+            Response val;
+            if (!topologyService.isZoneLeader() && zoneLeader != null) {
+                // query the zone leader
+                val = topologyService.sendSynchronous(zoneLeader, command, defaultTimeout);
+            } else {
+                // query the controller
+                val = topologyService.sendSynchronousToController(command, defaultTimeout);
+            }
+            ProvisionResponse response = (ProvisionResponse) val;
+            return response.getContributionUrl().openStream();
+        } catch (MessageException e) {
+            throw new ResolutionException(e);
+        } catch (IOException e) {
+            throw new ResolutionException(e);
+        }
+    }
+
+}
