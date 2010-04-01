8bf4c8eafe7312636a748da30e68737a15da0728
kpeasesvn <noreply@fabric3.org>
2010-04-01 21:36:04 +0000

    Implementation of WSDLLocator that delegates to the f3 Source to resolve InputSources for the base document and imports

---
+++ extension/interface/fabric3-interface-wsdl/src/main/java/org/fabric3/wsdl/contribution/impl/SourceWsdlLocator.java
@@ -0,0 +1,124 @@
+package org.fabric3.wsdl.contribution.impl;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.wsdl.xml.WSDLLocator;
+
+import org.fabric3.host.contribution.ValidationFailure;
+import org.fabric3.host.stream.Source;
+import org.fabric3.spi.introspection.IntrospectionContext;
+import org.xml.sax.InputSource;
+
+/**
+ * Implementation of javax.wsdl.xml.WSDLLocator that delegates import resolution
+ * to the Source 
+ */
+public class SourceWsdlLocator implements WSDLLocator 
+{
+	private Source mSource;
+	private IntrospectionContext mContext;
+	private String mLatestImportURI;
+	private List<InputStream> mStreams;
+	
+	/**
+	 * Constructor
+	 * 
+	 * @param aSource
+	 * @param aContext
+	 */
+	public SourceWsdlLocator(Source aSource, IntrospectionContext aContext) 
+	{
+		mSource = aSource;
+		mContext = aContext;
+		mStreams = new ArrayList();
+	}
+
+   /**
+    * @see javax.wsdl.xml.WSDLLocator#getBaseInputSource()
+    */
+	public InputSource getBaseInputSource() 
+	{
+		try
+		{
+			return new InputSource(addStream(mSource.openStream()));
+		}
+		catch (IOException ex)
+		{
+			mContext.addError(new InvalidWsdl(ex.getLocalizedMessage(), ex));
+			return null;
+		}
+	}
+
+   /**
+    * @see javax.wsdl.xml.WSDLLocator#getBaseURI()
+    */
+	public String getBaseURI() 
+	{
+		return mSource.getSystemId();
+	}
+
+   /**
+    * @see javax.wsdl.xml.WSDLLocator#getImportInputSource(java.lang.String, java.lang.String)
+    */
+    public InputSource getImportInputSource(String aParentLocation, String aImportLocation)
+	{
+    	try
+    	{
+    		Source importSource = mSource.getImportSource(aParentLocation, aImportLocation);
+    		if (importSource != null)
+    		{
+    			mLatestImportURI = importSource.getSystemId();
+    			return new InputSource(addStream(importSource.openStream()));
+    		}
+    	}
+    	catch (IOException ex)
+    	{
+			mContext.addError(new InvalidWsdl(ex.getLocalizedMessage(), ex));
+    	}
+		return null;
+	}
+
+   /**
+    * @see javax.wsdl.xml.WSDLLocator#getLatestImportURI()
+    */
+	public String getLatestImportURI() 
+	{
+		return mLatestImportURI;
+	}
+
+   /**
+    * @see javax.wsdl.xml.WSDLLocator#close()
+    */
+	public void close() 
+	{
+		// close all closeable streams we created for this locator
+		for (InputStream stream : mStreams)
+		{
+			try
+			{
+				stream.close();
+			}
+			catch(Throwable t)
+			{
+				// ignore since some may have been closed already
+			}
+		}
+	}
+	
+	/**
+	 * Adds a stream to the collection
+	 * @param aStream
+	 * @return stream that was given
+	 */
+	private InputStream addStream(InputStream aStream)
+	{
+		if (aStream != null)
+			mStreams.add(aStream);
+		return aStream;
+	}
+}
