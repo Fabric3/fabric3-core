eeb8d13729f956592138e62c5831c3354acb338f
jimmarino <jim.marino@gmail.com>
2010-04-08 23:57:39 +0000

    remove caching

--- kernel/impl/fabric3-contribution/src/main/java/org/fabric3/contribution/ClasspathProcessorRegistryImpl.java
+++ kernel/impl/fabric3-contribution/src/main/java/org/fabric3/contribution/ClasspathProcessorRegistryImpl.java
@@ -40,9 +40,7 @@ package org.fabric3.contribution;
 import java.io.IOException;
 import java.net.URL;
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 import org.osoa.sca.annotations.EagerInit;
 
@@ -55,8 +53,6 @@ import org.fabric3.spi.contribution.archive.ClasspathProcessorRegistry;
 @EagerInit
 public class ClasspathProcessorRegistryImpl implements ClasspathProcessorRegistry {
     private List<ClasspathProcessor> processors = new ArrayList<ClasspathProcessor>();
-    // cache of previously processed artifact URLs
-    private Map<URL, List<URL>> cache = new HashMap<URL, List<URL>>();
 
     public void register(ClasspathProcessor processor) {
         processors.add(processor);
@@ -67,23 +63,14 @@ public class ClasspathProcessorRegistryImpl implements ClasspathProcessorRegistr
     }
 
     public List<URL> process(URL url) throws IOException {
-        List<URL> cached = cache.get(url);
-        if (cached != null) {
-            // artifact has already been processed, reuse it
-            return cached;
-        }
         for (ClasspathProcessor processor : processors) {
             if (processor.canProcess(url)) {
-                List<URL> urls = processor.process(url);
-                cache.put(url, urls);
-                return urls;
-
+                return processor.process(url);
             }
         }
         // artifact does not need to be expanded, just return its base url
         List<URL> urls = new ArrayList<URL>();
         urls.add(url);
-        cache.put(url, urls);
         return urls;
     }
 }
