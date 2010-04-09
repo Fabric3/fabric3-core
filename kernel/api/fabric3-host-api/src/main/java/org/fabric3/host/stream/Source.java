61f96495af8f140b2e3b4876f3db730de1bf584a
jimmarino <jim.marino@gmail.com>
2010-04-09 10:08:49 +0000

    javadoc

--- kernel/api/fabric3-host-api/src/main/java/org/fabric3/host/stream/Source.java
+++ kernel/api/fabric3-host-api/src/main/java/org/fabric3/host/stream/Source.java
@@ -69,15 +69,14 @@ public interface Source {
      * @throws IOException if there is an error opening the stream
      */
     InputStream openStream() throws IOException;
-    
+
     /**
-     * Returns a Source for an imported artifact, resolving it's location
-     * relative to the parent location
-     * 
+     * Returns a Source for an imported artifact, resolving it's location relative to the parent location
+     *
      * @param aParentLocation base location URI
      * @param aImportLocation import location URI
      * @return an input stream or null if the import location cannot be resolved
-     * @throws IOException
+     * @throws IOException if there is an error returning the import source
      */
     Source getImportSource(String aParentLocation, String aImportLocation) throws IOException;
 }
