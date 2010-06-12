d51d22d1318dfe9807112d70919a90ec355d18bf
jimmarino <jim.marino@gmail.com>
2010-06-12 15:59:28 +0000

    add support for clustered timer singletons; split controller and participant composites

--- extension/implementation/fabric3-timer/src/main/java/org/fabric3/implementation/timer/runtime/TimerComponent.java
+++ extension/implementation/fabric3-timer/src/main/java/org/fabric3/implementation/timer/runtime/TimerComponent.java
@@ -99,6 +99,7 @@ public class TimerComponent extends JavaComponent implements TopologyListener {
     public void start() throws ComponentException {
         super.start();
         if (Scope.DOMAIN.equals(scope)) {
+            topologyService.register(this);
             if (RuntimeMode.PARTICIPANT == info.getRuntimeMode() && !topologyService.isZoneLeader()) {
                 // defer scheduling until this node becomes zone leader
                 return;
@@ -109,6 +110,9 @@ public class TimerComponent extends JavaComponent implements TopologyListener {
 
     public void stop() throws ComponentException {
         super.stop();
+        if (Scope.DOMAIN.equals(scope)) {
+            topologyService.deregister(this);
+        }
         if (future != null && !future.isCancelled() && !future.isDone()) {
             future.cancel(true);
         }
