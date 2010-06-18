81f78d3364b770ccaf3ff6fc73ab8dafbc50e5d6
jimmarino <jim.marino@gmail.com>
2010-06-18 03:40:11 +0000

    package shift

--- extension/other/fabric3-resource/src/main/java/org/fabric3/resource/runtime/SystemSourcedResourceWireAttacher.java
+++ extension/other/fabric3-resource/src/main/java/org/fabric3/resource/runtime/SystemSourcedResourceWireAttacher.java
@@ -35,7 +35,7 @@
 * GNU General Public License along with Fabric3.
 * If not, see <http://www.gnu.org/licenses/>.
 */
-package org.fabric3.resource.runtime.wire;
+package org.fabric3.resource.runtime;
 
 import java.net.URI;
 
