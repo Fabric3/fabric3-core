69a509cbd4eb89d7e35d8f2ef3152b8c67e91d62
jimmarino <jim.marino@gmail.com>
2010-07-30 19:32:48 +0000

    cleanup imports

--- runtime/tomcat/fabric3-tomcat-extension/src/main/java/org/fabric3/runtime/tomcat/servlet/Fabric3DispatchingServlet.java
+++ runtime/tomcat/fabric3-tomcat-extension/src/main/java/org/fabric3/runtime/tomcat/servlet/Fabric3DispatchingServlet.java
@@ -43,8 +43,6 @@ import java.util.concurrent.ConcurrentHashMap;
 import javax.servlet.Servlet;
 import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
-import javax.servlet.ServletRequest;
-import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
