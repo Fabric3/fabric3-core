9ff9dda9e29a638860c64b4de1c0c2dfb2f74e53
jimmarino <jim.marino@gmail.com>
2011-03-27 07:26:36 +0000

    add domain testcases

---
+++ kernel/impl/fabric3-fabric/src/test/java/org/fabric3/fabric/domain/DistributedDomainTestCase.java
@@ -0,0 +1,253 @@
+/*
+ * Fabric3
+ * Copyright (c) 2009-2011 Metaform Systems
+ *
+ * Fabric3 is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation, either version 3 of
+ * the License, or (at your option) any later version, with the
+ * following exception:
+ *
+ * Linking this software statically or dynamically with other
+ * modules is making a combined work based on this software.
+ * Thus, the terms and conditions of the GNU General Public
+ * License cover the whole combination.
+ *
+ * As a special exception, the copyright holders of this software
+ * give you permission to link this software with independent
+ * modules to produce an executable, regardless of the license
+ * terms of these independent modules, and to copy and distribute
+ * the resulting executable under terms of your choice, provided
+ * that you also meet, for each linked independent module, the
+ * terms and conditions of the license of that module. An
+ * independent module is a module which is not derived from or
+ * based on this software. If you modify this software, you may
+ * extend this exception to your version of the software, but
+ * you are not obligated to do so. If you do not wish to do so,
+ * delete this exception statement from your version.
+ *
+ * Fabric3 is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty
+ * of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the
+ * GNU General Public License along with Fabric3.
+ * If not, see <http://www.gnu.org/licenses/>.
+ *
+ * ----------------------------------------------------
+ *
+ * Portions originally based on Apache Tuscany 2007
+ * licensed under the Apache 2.0 license.
+ *
+ */
+package org.fabric3.fabric.domain;
+
+import java.net.URI;
+import javax.xml.namespace.QName;
+
+import junit.framework.TestCase;
+import org.easymock.EasyMock;
+import org.easymock.IAnswer;
+import org.easymock.IMocksControl;
+
+import org.fabric3.fabric.binding.BindingSelector;
+import org.fabric3.fabric.collector.Collector;
+import org.fabric3.fabric.collector.CollectorImpl;
+import org.fabric3.fabric.instantiator.InstantiationContext;
+import org.fabric3.fabric.instantiator.LogicalModelInstantiator;
+import org.fabric3.fabric.lcm.LogicalComponentManagerImpl;
+import org.fabric3.host.RuntimeMode;
+import org.fabric3.host.contribution.Deployable;
+import org.fabric3.host.domain.DeploymentException;
+import org.fabric3.host.runtime.DefaultHostInfo;
+import org.fabric3.host.runtime.HostInfo;
+import org.fabric3.model.type.component.ComponentDefinition;
+import org.fabric3.model.type.component.Composite;
+import org.fabric3.spi.contribution.Contribution;
+import org.fabric3.spi.contribution.ContributionState;
+import org.fabric3.spi.contribution.MetaDataStore;
+import org.fabric3.spi.contribution.Resource;
+import org.fabric3.spi.contribution.ResourceElement;
+import org.fabric3.spi.contribution.manifest.QNameSymbol;
+import org.fabric3.spi.domain.Deployer;
+import org.fabric3.spi.domain.DeploymentPackage;
+import org.fabric3.spi.generator.Deployment;
+import org.fabric3.spi.generator.Generator;
+import org.fabric3.spi.generator.policy.PolicyAttacher;
+import org.fabric3.spi.model.instance.LogicalComponent;
+import org.fabric3.spi.model.instance.LogicalCompositeComponent;
+
+/**
+ * @version $Rev$ $Date$
+ */
+public class DistributedDomainTestCase extends TestCase {
+    private static final URI COMPONENT_URI = URI.create("fabric3://domain/component");
+    private static final QName DEPLOYABLE = new QName("foo", "bar");
+
+    private IMocksControl control;
+    private DistributedDomain domain;
+    private MetaDataStore store;
+    private LogicalModelInstantiator instantiator;
+    private PolicyAttacher policyAttacher;
+    private BindingSelector bindingSelector;
+    private Generator generator;
+    private Deployer deployer;
+    private ComponentDefinition componentDefinition;
+    private LogicalComponentManagerImpl lcm;
+
+    public void testInclude() throws Exception {
+
+        Composite composite = createComposite();
+
+        IAnswer<InstantiationContext> answer = createAnswer();
+        EasyMock.expect(instantiator.include(EasyMock.eq(composite), EasyMock.isA(LogicalCompositeComponent.class))).andStubAnswer(answer);
+
+        policyAttacher.attachPolicies(EasyMock.isA(LogicalCompositeComponent.class), EasyMock.anyBoolean());
+        bindingSelector.selectBindings(EasyMock.isA(LogicalCompositeComponent.class));
+
+        Deployment deployment = new Deployment("1");
+        EasyMock.expect(generator.generate(EasyMock.isA(LogicalCompositeComponent.class), EasyMock.anyBoolean())).andReturn(deployment);
+        deployer.deploy(EasyMock.isA(DeploymentPackage.class));
+
+        control.replay();
+
+        domain.include(DEPLOYABLE);
+
+        // verify the component contained in the composite was added to the logical model
+        assertNotNull(lcm.getRootComponent().getComponent(COMPONENT_URI));
+        control.verify();
+    }
+
+    public void testIncludeAndRemove() throws Exception {
+        Composite composite = createComposite();
+
+        IAnswer<InstantiationContext> answer = createAnswer();
+        EasyMock.expect(instantiator.include(EasyMock.eq(composite), EasyMock.isA(LogicalCompositeComponent.class))).andStubAnswer(answer);
+
+        policyAttacher.attachPolicies(EasyMock.isA(LogicalCompositeComponent.class), EasyMock.anyBoolean());
+        bindingSelector.selectBindings(EasyMock.isA(LogicalCompositeComponent.class));
+
+        Deployment deployment = new Deployment("1");
+        EasyMock.expect(generator.generate(EasyMock.isA(LogicalCompositeComponent.class), EasyMock.anyBoolean())).andReturn(deployment).times(2);
+        deployer.deploy(EasyMock.isA(DeploymentPackage.class));
+        EasyMock.expectLastCall().times(2);
+        control.replay();
+
+        domain.include(DEPLOYABLE);
+
+        assertNotNull(lcm.getRootComponent().getComponent(COMPONENT_URI));
+
+        domain.undeploy(composite.getContributionUri(), false);
+        // verify the component contained in the composite was added to the logical model
+        assertNull(lcm.getRootComponent().getComponent(COMPONENT_URI));
+        control.verify();
+    }
+
+    @SuppressWarnings({"ThrowableInstanceNeverThrown"})
+    public void testDeploymentError() throws Exception {
+        Composite composite = createComposite();
+
+        IAnswer<InstantiationContext> answer = createAnswer();
+        EasyMock.expect(instantiator.include(EasyMock.eq(composite), EasyMock.isA(LogicalCompositeComponent.class))).andStubAnswer(answer);
+
+        policyAttacher.attachPolicies(EasyMock.isA(LogicalCompositeComponent.class), EasyMock.anyBoolean());
+        bindingSelector.selectBindings(EasyMock.isA(LogicalCompositeComponent.class));
+
+        Deployment deployment = new Deployment("1");
+        EasyMock.expect(generator.generate(EasyMock.isA(LogicalCompositeComponent.class), EasyMock.anyBoolean())).andReturn(deployment);
+        deployer.deploy(EasyMock.isA(DeploymentPackage.class));
+        // simulate a deployment exception
+        EasyMock.expectLastCall().andThrow(new DeploymentException());
+
+        control.replay();
+
+        try {
+            domain.include(DEPLOYABLE);
+            fail();
+        } catch (DeploymentException e) {
+            // expected
+        }
+        // verify the component contained in the composite was *not* added to the logical model
+        assertNull(lcm.getRootComponent().getComponent(COMPONENT_URI));
+        control.verify();
+    }
+
+    @Override
+    protected void setUp() throws Exception {
+        super.setUp();
+        control = EasyMock.createControl();
+        store = control.createMock(MetaDataStore.class);
+
+        URI uri = URI.create("fabric3://domain");
+        HostInfo info = new DefaultHostInfo("runtime", RuntimeMode.VM, uri, null, null, null, null, null, null, null, null);
+
+        ContributionHelperImpl helper = new ContributionHelperImpl(store, info);
+
+        lcm = new LogicalComponentManagerImpl(info);
+        lcm.init();
+
+        generator = control.createMock(Generator.class);
+        instantiator = control.createMock(LogicalModelInstantiator.class);
+
+        policyAttacher = control.createMock(PolicyAttacher.class);
+        bindingSelector = control.createMock(BindingSelector.class);
+        deployer = control.createMock(Deployer.class);
+        Collector collector = new CollectorImpl();
+        domain = new DistributedDomain(store,
+                                       lcm,
+                                       generator,
+                                       instantiator,
+                                       policyAttacher,
+                                       bindingSelector,
+                                       deployer,
+                                       collector,
+                                       helper,
+                                       info);
+        domain.setTransactional(true);
+    }
+
+    private IAnswer<InstantiationContext> createAnswer() {
+        return new IAnswer<InstantiationContext>() {
+
+            @SuppressWarnings({"unchecked"})
+            public InstantiationContext answer() throws Throwable {
+                LogicalCompositeComponent domainComposite = (LogicalCompositeComponent) EasyMock.getCurrentArguments()[1];
+
+                LogicalComponent logicalComponent = new LogicalComponent(COMPONENT_URI, componentDefinition, domainComposite);
+                logicalComponent.setDeployable(DEPLOYABLE);
+                domainComposite.addComponent(logicalComponent);
+                return new InstantiationContext();
+            }
+        };
+    }
+
+    @SuppressWarnings({"unchecked"})
+    private Composite createComposite() {
+        Contribution contribution = createContribution();
+        Composite composite = new Composite(DEPLOYABLE);
+        componentDefinition = new ComponentDefinition("component");
+        composite.add(componentDefinition);
+        composite.setContributionUri(contribution.getUri());
+
+        QNameSymbol symbol = new QNameSymbol(DEPLOYABLE);
+        ResourceElement<QNameSymbol, Composite> element = new ResourceElement<QNameSymbol, Composite>(symbol, composite);
+        Resource resource = new Resource(contribution, null, "jar");
+        element.setResource(resource);
+
+        Deployable deployable = new Deployable(DEPLOYABLE);
+        contribution.getManifest().addDeployable(deployable);
+        EasyMock.expect(store.find(Composite.class, symbol)).andReturn(element).anyTimes();
+        return composite;
+    }
+
+    private Contribution createContribution() {
+        URI uri = URI.create("contribution");
+        Contribution contribution = new Contribution(uri);
+        contribution.setState(ContributionState.INSTALLED);
+
+        EasyMock.expect(store.find(uri)).andReturn(contribution).anyTimes();
+        return contribution;
+    }
+
+}
