826c209a5f6796cb80fd50321c873275019a6393
jimmarino <jim.marino@gmail.com>
2011-03-30 07:10:59 +0000

    more test coverage

--- kernel/impl/fabric3-fabric/src/main/java/org/fabric3/fabric/generator/wire/PhysicalOperationGenerator.java
+++ kernel/impl/fabric3-fabric/src/main/java/org/fabric3/fabric/generator/wire/PhysicalOperationGenerator.java
@@ -57,11 +57,11 @@ public interface PhysicalOperationGenerator {
      * Generates a PhysicalOperationDefinition when the source reference and target service contracts are the same.
      *
      * @param operations the logical operations to generate from
-     * @param remote  true if the interceptor chain handles remote invocations - i.e. it is for a bound service, bound reference or inter-process
-     *                wire.
+     * @param remote     true if the interceptor chain handles remote invocations - i.e. it is for a bound service, bound reference or inter-process
+     *                   wire.
      * @param result     resolved policy metadata
      * @return the PhysicalOperationDefinition
-     * @throws GenerationException if there is an error generatoring the operations
+     * @throws GenerationException if there is an error generating the operations
      */
     Set<PhysicalOperationDefinition> generateOperations(List<LogicalOperation> operations, boolean remote, PolicyResult result)
             throws GenerationException;
@@ -76,7 +76,7 @@ public interface PhysicalOperationGenerator {
      *                wire.
      * @param result  resolved policy metadata
      * @return the PhysicalOperationDefinition
-     * @throws GenerationException if there is an error generatoring the operations
+     * @throws GenerationException if there is an error generating the operations
      */
     Set<PhysicalOperationDefinition> generateOperations(List<LogicalOperation> sources,
                                                         List<LogicalOperation> targets,
