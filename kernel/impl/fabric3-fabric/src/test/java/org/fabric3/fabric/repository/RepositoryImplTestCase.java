0087e9b64ac32c63d5d2a5157b3b9b7d4bdf42d2
jimmarino <jim.marino@gmail.com>
2011-04-02 00:20:22 +0000

    repository test cases

--- kernel/impl/fabric3-fabric/src/test/java/org/fabric3/fabric/repository/RepositoryImplTestCase.java
+++ kernel/impl/fabric3-fabric/src/test/java/org/fabric3/fabric/repository/RepositoryImplTestCase.java
@@ -39,6 +39,7 @@ package org.fabric3.fabric.repository;
 
 import java.io.ByteArrayInputStream;
 import java.io.File;
+import java.io.IOException;
 import java.io.InputStream;
 import java.net.URI;
 import java.net.URL;
@@ -52,6 +53,9 @@ import org.fabric3.host.util.FileHelper;
 
 public class RepositoryImplTestCase extends TestCase {
     private RepositoryImpl repository;
+    private File runtimeDir;
+    private File sharedDir;
+    private File userDir;
 
     public void testStoreAndFind() throws Exception {
         URI uri = URI.create("test-resource");
@@ -59,6 +63,14 @@ public class RepositoryImplTestCase extends TestCase {
         repository.store(uri, archiveStream, false);
         URL contributionURL = repository.find(uri);
         assertNotNull(contributionURL);
+    }
+
+    public void testInit() throws Exception {
+        createArchives();
+        repository.init();
+        assertTrue(repository.exists(URI.create("testRuntime.jar")));
+        assertTrue(repository.exists(URI.create("testShared.jar")));
+        assertTrue(repository.exists(URI.create("testUser.jar")));
     }
 
     public void testList() throws Exception {
@@ -75,14 +87,23 @@ public class RepositoryImplTestCase extends TestCase {
         assertTrue(found);
     }
 
+    public void testRemove() throws Exception {
+        URI archiveUri = URI.create("test-resource");
+        InputStream archiveStream = new ByteArrayInputStream("test".getBytes());
+        repository.store(archiveUri, archiveStream, false);
+        assertTrue(new File(userDir, "test-resource").exists());
+        repository.remove(archiveUri);
+        assertFalse(new File(userDir, "test-resource").exists());
+    }
+
     protected void setUp() throws Exception {
         super.setUp();
-        File repository = new File("repository");
-        File runtimeDir = new File(repository, "runtime");
-        File sharedDir = new File(repository, "shared");
-        File userDir = new File(repository, "user");
+        File repositoryDir = new File("repository");
+        runtimeDir = new File(repositoryDir, "runtime");
+        sharedDir = new File(repositoryDir, "shared");
+        userDir = new File(repositoryDir, "user");
 
-        FileHelper.forceMkdir(new File(repository, "cache"));
+        FileHelper.forceMkdir(new File(repositoryDir, "cache"));
         FileHelper.forceMkdir(runtimeDir);
         FileHelper.forceMkdir(sharedDir);
         FileHelper.forceMkdir(userDir);
@@ -93,7 +114,7 @@ public class RepositoryImplTestCase extends TestCase {
         EasyMock.expect(info.getTempDir()).andReturn(null).atLeastOnce();
         EasyMock.expect(info.getRuntimeMode()).andReturn(RuntimeMode.VM).atLeastOnce();
         EasyMock.replay(info);
-        this.repository = new RepositoryImpl(info);
+        repository = new RepositoryImpl(info);
     }
 
     @Override
@@ -101,4 +122,13 @@ public class RepositoryImplTestCase extends TestCase {
         super.tearDown();
         FileHelper.forceDelete(new File("repository"));
     }
+
+    private void createArchives() throws IOException {
+        InputStream stream = new ByteArrayInputStream("test".getBytes());
+        FileHelper.write(stream, new File(runtimeDir, "testRuntime.jar"));
+        FileHelper.write(stream, new File(sharedDir, "testShared.jar"));
+        FileHelper.write(stream, new File(userDir, "testUser.jar"));
+    }
+
+
 }
