beb2f26582efe4426def2fb3e923003466774650
jimmarino <jim.marino@gmail.com>
2011-09-07 10:47:05 +0000

    add target wire attacher

---
+++ extension/implementation/drools/fabric3-drools/src/main/java/org/fabric3/implementation/drools/runtime/DroolsTargetWireAttacher.java
@@ -0,0 +1,97 @@
+/*
+* Fabric3
+* Copyright (c) 2009-2011 Metaform Systems
+*
+* Fabric3 is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as
+* published by the Free Software Foundation, either version 3 of
+* the License, or (at your option) any later version, with the
+* following exception:
+*
+* Linking this software statically or dynamically with other
+* modules is making a combined work based on this software.
+* Thus, the terms and conditions of the GNU General Public
+* License cover the whole combination.
+*
+* As a special exception, the copyright holders of this software
+* give you permission to link this software with independent
+* modules to produce an executable, regardless of the license
+* terms of these independent modules, and to copy and distribute
+* the resulting executable under terms of your choice, provided
+* that you also meet, for each linked independent module, the
+* terms and conditions of the license of that module. An
+* independent module is a module which is not derived from or
+* based on this software. If you modify this software, you may
+* extend this exception to your version of the software, but
+* you are not obligated to do so. If you do not wish to do so,
+* delete this exception statement from your version.
+*
+* Fabric3 is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty
+* of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+* See the GNU General Public License for more details.
+*
+* You should have received a copy of the
+* GNU General Public License along with Fabric3.
+* If not, see <http://www.gnu.org/licenses/>.
+*/
+package org.fabric3.implementation.drools.runtime;
+
+import java.net.URI;
+
+import org.osoa.sca.annotations.EagerInit;
+import org.osoa.sca.annotations.Reference;
+
+import org.fabric3.implementation.drools.provision.DroolsTargetDefinition;
+import org.fabric3.spi.builder.WiringException;
+import org.fabric3.spi.builder.component.TargetWireAttacher;
+import org.fabric3.spi.builder.component.WireAttachException;
+import org.fabric3.spi.classloader.ClassLoaderRegistry;
+import org.fabric3.spi.cm.ComponentManager;
+import org.fabric3.spi.model.physical.PhysicalSourceDefinition;
+import org.fabric3.spi.objectfactory.ObjectFactory;
+import org.fabric3.spi.util.UriHelper;
+import org.fabric3.spi.wire.InvocationChain;
+import org.fabric3.spi.wire.Wire;
+
+/**
+ * Attaches and detaches wires to and from Drools components.
+ *
+ * @version $Rev$ $Date$
+ */
+@EagerInit
+public class DroolsTargetWireAttacher implements TargetWireAttacher<DroolsTargetDefinition> {
+    private ComponentManager manager;
+    private ClassLoaderRegistry classLoaderRegistry;
+
+    public DroolsTargetWireAttacher(@Reference ComponentManager manager, @Reference ClassLoaderRegistry classLoaderRegistry) {
+        this.manager = manager;
+        this.classLoaderRegistry = classLoaderRegistry;
+    }
+
+    public void attach(PhysicalSourceDefinition sourceDefinition, DroolsTargetDefinition targetDefinition, Wire wire) throws WireAttachException {
+        URI targetName = UriHelper.getDefragmentedName(targetDefinition.getUri());
+        DroolsComponent component = (DroolsComponent) manager.getComponent(targetName);
+        if (component == null) {
+            throw new WireAttachException("Target not found: " + targetName);
+        }
+        ClassLoader classLoader = classLoaderRegistry.getClassLoader(targetDefinition.getClassLoaderId());
+        StatelessDroolsInterceptor interceptor = new StatelessDroolsInterceptor(component, classLoader);
+
+        // attach the interceptor to forward invocation chains
+        for (InvocationChain chain : wire.getInvocationChains()) {
+            chain.addInterceptor(interceptor);
+        }
+    }
+
+    public void detach(PhysicalSourceDefinition source, DroolsTargetDefinition target) throws WiringException {
+        // no-op
+    }
+
+    public ObjectFactory<?> createObjectFactory(DroolsTargetDefinition definition) throws WiringException {
+        URI targetId = UriHelper.getDefragmentedName(definition.getUri());
+        DroolsComponent targetComponent = (DroolsComponent) manager.getComponent(targetId);
+        return targetComponent.createObjectFactory();
+    }
+
+}
