fd7c92fcc86d848953de694c42a6d88ee1554f43
jimmarino <jim.marino@gmail.com>
2011-09-12 08:08:28 +0000

    move package

--- kernel/impl/fabric3-introspection-xml/src/main/java/org/fabric3/introspection/xml/template/TemplateRegistryImpl.java
+++ kernel/impl/fabric3-introspection-xml/src/main/java/org/fabric3/introspection/xml/template/TemplateRegistryImpl.java
@@ -35,7 +35,7 @@
  * GNU General Public License along with Fabric3.
  * If not, see <http://www.gnu.org/licenses/>.
 */
-package org.fabric3.introspection.xml;
+package org.fabric3.introspection.xml.template;
 
 import java.net.URI;
 import java.util.Iterator;
