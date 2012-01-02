a6d255392866bc51a7b9ae38a792c2eb3b5b9584
jimmarino <jim.marino@gmail.com>
2012-01-02 10:31:44 +0000

    change error message

--- kernel/api/fabric3-spi/src/main/java/org/fabric3/spi/classloader/MultiClassLoaderObjectInputStream.java
+++ kernel/api/fabric3-spi/src/main/java/org/fabric3/spi/classloader/MultiClassLoaderObjectInputStream.java
@@ -71,7 +71,7 @@ public class MultiClassLoaderObjectInputStream extends ObjectInputStream {
             URI uri = URI.create(id);
             ClassLoader cl = registry.getClassLoader(uri);
             if (cl == null) {
-                throw new IOException("Contribution not installed. Classloader not found: " + id);
+                throw new IOException("Contribution not installed: " + id);
             }
             return Class.forName(desc.getName(), false, cl);
         }
