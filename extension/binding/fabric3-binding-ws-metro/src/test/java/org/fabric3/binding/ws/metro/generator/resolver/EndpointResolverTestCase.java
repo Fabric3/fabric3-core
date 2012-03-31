f4a45340402e3001024b8020732864c4e12bc909
palmalcheg <palmal.mozhay@gmail.com>
2012-03-31 14:02:00 +0000

    fix SOAP endpoint resolver , test added for SOAP 1.1

--- extension/binding/fabric3-binding-ws-metro/src/test/java/org/fabric3/binding/ws/metro/generator/resolver/EndpointResolverTestCase.java
+++ extension/binding/fabric3-binding-ws-metro/src/test/java/org/fabric3/binding/ws/metro/generator/resolver/EndpointResolverTestCase.java
@@ -32,5 +32,24 @@ public class EndpointResolverTestCase extends TestCase {
 		ServiceEndpointDefinition def1 = resolver.resolveServiceEndpoint(wsdlElement, wsdl);
 		assertNotNull(def1.getServicePath());	
     }
+	
+	public void testWsdlSOAP11() throws Exception {
+		Wsdl4JFactoryImpl wsdlFactory = new Wsdl4JFactoryImpl();
+		Definition wsdl = wsdlFactory.newReader().readWSDL("src/test/resources/WeatherClientSide.wsdl");
+		EndpointResolverImpl resolver = new EndpointResolverImpl(wsdlFactory);
+		
+		Service service = (Service)wsdl.getServices().values().iterator().next();
+		QName serviceQ = service.getQName();		
+		String port = ((Port)service.getPorts().values().iterator().next()).getName();
+		
+		QName portQ = new QName(serviceQ.getNamespaceURI(), port);		
+		WsdlElement wsdlElement = new WsdlElement(serviceQ, portQ);
+				
+		ServiceEndpointDefinition def1 = resolver.resolveServiceEndpoint(wsdlElement, wsdl);
+		assertNotNull(def1.getServicePath());	
+		
+		ReferenceEndpointDefinition def = resolver.resolveReferenceEndpoint(wsdlElement, wsdl);
+		assertNotNull(def.getUrl());
+    }
 
 }
