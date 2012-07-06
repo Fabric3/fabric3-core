e28cdedc1e39fa4e7474a687f5aedbb722d8f3ad
jimmarino <jim.marino@gmail.com>
2012-07-06 12:50:14 +0000

    changes to the Tomcat7 F3 host: restrict restart to those with admin role and disable by default; add support for booting the runtime in distributed (controller, participant) modes; make startup synchronous and handle exceptions

--- runtime/tomcat/fabric3-tomcat-host/src/main/java/org/fabric3/tomcat/host/Fabric3HostServlet.java
+++ runtime/tomcat/fabric3-tomcat-host/src/main/java/org/fabric3/tomcat/host/Fabric3HostServlet.java
@@ -1,10 +1,49 @@
+/*
+ * Fabric3
+ * Copyright (c) 2009-2012 Metaform Systems
+ *
+ * Fabric3 is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation, either version 3 of
+ * the License, or (at your option) any later version, with the
+ * following exception:
+ *
+ * Linking this software statically or dynamically with other
+ * modules is making a combined work based on this software.
+ * Thus, the terms and conditions of the GNU General Public
+ * License cover the whole combination.
+ *
+ * As a special exception, the copyright holders of this software
+ * give you permission to link this software with independent
+ * modules to produce an executable, regardless of the license
+ * terms of these independent modules, and to copy and distribute
+ * the resulting executable under terms of your choice, provided
+ * that you also meet, for each linked independent module, the
+ * terms and conditions of the license of that module. An
+ * independent module is a module which is not derived from or
+ * based on this software. If you modify this software, you may
+ * extend this exception to your version of the software, but
+ * you are not obligated to do so. If you do not wish to do so,
+ * delete this exception statement from your version.
+ *
+ * Fabric3 is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty
+ * of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the
+ * GNU General Public License along with Fabric3.
+ * If not, see <http://www.gnu.org/licenses/>.
+*/
 package org.fabric3.tomcat.host;
 
 import java.io.File;
 import java.io.FileFilter;
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.net.URI;
 import java.net.URL;
+import java.security.Principal;
 import java.util.ArrayList;
 import java.util.List;
 import javax.management.MBeanServer;
@@ -18,6 +57,7 @@ import org.apache.catalina.ContainerServlet;
 import org.apache.catalina.Context;
 import org.apache.catalina.Engine;
 import org.apache.catalina.Host;
+import org.apache.catalina.Realm;
 import org.apache.catalina.Service;
 import org.apache.catalina.Wrapper;
 import org.apache.catalina.mbeans.MBeanUtils;
@@ -25,6 +65,7 @@ import org.w3c.dom.Document;
 
 import org.fabric3.api.annotation.monitor.Info;
 import org.fabric3.api.annotation.monitor.Severe;
+import org.fabric3.host.Fabric3Exception;
 import org.fabric3.host.RuntimeMode;
 import org.fabric3.host.classloader.MaskingClassLoader;
 import org.fabric3.host.monitor.MonitorEventDispatcher;
@@ -50,26 +91,52 @@ import static org.fabric3.host.runtime.BootConstants.RUNTIME_MONITOR;
 import static org.fabric3.host.runtime.BootstrapHelper.createHostInfo;
 
 public class Fabric3HostServlet extends HttpServlet implements ContainerServlet {
+    private static final long serialVersionUID = 2298727517016404860L;
 
     private static final String CATALINA_BASE_PROP = "catalina.base";
-
-    private static final long serialVersionUID = 1L;
-
-    private static final String RUNTIME_MODE = "vm";
-
+    private static final String F3_RESTART_PROP = "fabric3.restart";
+    private static final String FABRIC3_MODE = "fabric3.mode";
     private static final String FABRIC3_HOME = "fabric3";
 
     private RuntimeCoordinator coordinator;
     private ServerMonitor monitor;
-
     private Wrapper wrapper;
-
     private Host host;
-
     private File installDirectory;
+    private boolean restartEnabled;
 
-    private synchronized void init(Service service, String runtimeId, boolean firstInitTime) {
+    public Wrapper getWrapper() {
+        return wrapper;
+    }
+
+    public void setWrapper(Wrapper wrapper) {
+        this.wrapper = wrapper;
+        if (wrapper != null) {
+            Context context = (Context) wrapper.getParent();
+            host = (Host) context.getParent();
+        }
+    }
+
+    public void init(ServletConfig config) throws ServletException {
+        super.init(config);
+        if (host == null) {
+            throw new ServletException("Tomcat host not set");
+        }
+        Engine engine = (Engine) host.getParent();
+        init(engine.getService(), true);
+    }
+
+    public void destroy() {
+        super.destroy();
+        stop();
+    }
+
+
+    private synchronized void init(Service service, boolean firstInitTime) throws ServletException {
         try {
+            String runtimeId = System.getProperty(FABRIC3_MODE, "vm").toLowerCase();
+
+            restartEnabled = Boolean.parseBoolean(System.getProperty(F3_RESTART_PROP));
 
             String catalinaInstall = System.getProperty(CATALINA_BASE_PROP);
             File catalinaInstallDir = new File(catalinaInstall);
@@ -77,13 +144,12 @@ public class Fabric3HostServlet extends HttpServlet implements ContainerServlet 
             if (firstInitTime) {
                 installDirectory = findInstallationDirectory(catalinaInstallDir);
             } else if (installDirectory == null) {
-                throw new Fabric3HostException("No Fabric3 installation found.");
+                throw new ServletException("No Fabric3 installation found.");
             }
 
             File extensionsDir = new File(installDirectory, "extensions");
 
-            // calculate config directories based on the mode the runtime is
-            // booted in
+            // calculate config directories based on the mode the runtime is booted in
             File runtimeDir = getRuntimeDirectory(installDirectory, runtimeId);
             File configDir = BootstrapHelper.getDirectory(runtimeDir, "config");
 
@@ -154,12 +220,10 @@ public class Fabric3HostServlet extends HttpServlet implements ContainerServlet 
             MonitorProxyService monitorService = runtime.getComponent(MonitorProxyService.class, MONITOR_FACTORY_URI);
             monitor = monitorService.createMonitor(ServerMonitor.class, RUNTIME_MONITOR_CHANNEL_URI);
             monitor.started(mode.toString());
-        } catch (Exception e) {
-            if (monitor != null) {
-                // there could have been an error initializing the monitor
-                monitor.runError(e);
-            }
-            throw new Fabric3HostException(e);
+        } catch (Fabric3Exception e) {
+            throw new ServletException(e);
+        } catch (IOException e) {
+            throw new ServletException(e);
         }
     }
 
@@ -169,9 +233,8 @@ public class Fabric3HostServlet extends HttpServlet implements ContainerServlet 
                 monitor.stopped();
                 coordinator.shutdown();
             }
-        } catch (ShutdownException ex) {
-            monitor.runError(ex);
-            throw new Fabric3HostException(ex);
+        } catch (ShutdownException e) {
+            monitor.runError(e);
         }
     }
 
@@ -182,18 +245,6 @@ public class Fabric3HostServlet extends HttpServlet implements ContainerServlet 
             throw new IllegalArgumentException("Runtime directory does not exist:" + runtimeDir);
         }
         return runtimeDir;
-    }
-
-    public Wrapper getWrapper() {
-        return wrapper;
-    }
-
-    public void setWrapper(Wrapper wrapper) {
-        this.wrapper = wrapper;
-        if (wrapper != null) {
-            Context context = (Context) wrapper.getParent();
-            host = (Host) context.getParent();
-        }
     }
 
     public interface ServerMonitor {
@@ -209,46 +260,66 @@ public class Fabric3HostServlet extends HttpServlet implements ContainerServlet 
 
     }
 
-    @Override
-    public void init(ServletConfig config) throws ServletException {
-        super.init(config);
-        new Thread(new Runnable() {
-            public void run() {
-                if (host != null) {
-                    Engine engine = (Engine) host.getParent();
-                    init(engine.getService(), RUNTIME_MODE, true);
-                }
-            }
-        }).start();
-    }
-
-    @Override
-    public void destroy() {
-        super.destroy();
-        stop();
-    }
-
-    @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         super.doGet(req, resp);
-        String restart = req.getParameter("restart");
-        String runtime_mode = req.getParameter("runtime_mode");
-        String f3_home_path = req.getParameter("fabric3_home");
 
-        runtime_mode = runtime_mode == null ? RUNTIME_MODE : runtime_mode;
-        f3_home_path = f3_home_path == null ? FABRIC3_HOME : f3_home_path;
+        if (!restartEnabled) {
+            notAuthorized(resp);
+            return;
+        }
 
-        if (restart != null && Boolean.parseBoolean(restart)) {
+        if (!hasAdminRole(req, resp)) {
+            return;
+        }
+
+        boolean restart = Boolean.parseBoolean(req.getParameter("restart"));
+
+        if (restart) {
             destroy();
             Engine engine = (Engine) host.getParent();
-            init(engine.getService(), runtime_mode, false);
+            init(engine.getService(), false);
         }
 
     }
 
-    @Override
-    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        doGet(req, resp);
+    private boolean hasAdminRole(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+        String username = req.getParameter("restart");
+        String password = req.getParameter("password");
+        if (username == null || password == null) {
+            notAuthenticated(resp);
+            return false;
+        }
+        Realm realm = getWrapper().getRealm();
+        if (realm != null) {
+            Principal principal = realm.authenticate(username, password);
+            if (principal != null) {
+                if (!realm.hasRole(principal, "admin-gui") && !realm.hasRole(principal, "admin-script")) {
+                    notAuthorized(resp);
+                }
+            } else {
+                notAuthorized(resp);
+            }
+
+
+        } else {
+            notAuthorized(resp);
+            return false;
+        }
+        return true;
+    }
+
+    private void notAuthenticated(HttpServletResponse resp) throws IOException {
+        resp.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+        PrintWriter writer = resp.getWriter();
+        writer.print("No Authentication Credentials");
+        writer.close();
+    }
+
+    private void notAuthorized(HttpServletResponse resp) throws IOException {
+        resp.setStatus(HttpServletResponse.SC_FORBIDDEN);
+        PrintWriter writer = resp.getWriter();
+        writer.print("Not Authorized");
+        writer.close();
     }
 
     private File findInstallationDirectory(File currentDirectory) {
