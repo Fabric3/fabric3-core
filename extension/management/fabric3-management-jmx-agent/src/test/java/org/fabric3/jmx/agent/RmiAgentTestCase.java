a3ed90c2e366c8abe95bb67fb8fc8d6e1dc7e3bd
jimmarino <jim.marino@gmail.com>
2012-10-23 22:09:44 +0000

    skip test if port is bound

--- extension/management/fabric3-management-jmx-agent/src/test/java/org/fabric3/jmx/agent/RmiAgentTestCase.java
+++ extension/management/fabric3-management-jmx-agent/src/test/java/org/fabric3/jmx/agent/RmiAgentTestCase.java
@@ -18,6 +18,7 @@
  */
 package org.fabric3.jmx.agent;
 
+import java.rmi.server.ExportException;
 import javax.management.MBeanServer;
 
 import junit.framework.TestCase;
@@ -33,49 +34,69 @@ public class RmiAgentTestCase extends TestCase {
     private static final int PORT = 1050;
 
     public void testConfiguredPort() throws Exception {
-        MBeanServer mBeanServer = EasyMock.createNiceMock(MBeanServer.class);
-        DelegatingJmxAuthenticator authenticator = EasyMock.createNiceMock(DelegatingJmxAuthenticator.class);
-        RmiAgentMonitor monitor = EasyMock.createNiceMock(RmiAgentMonitor.class);
+        try {
+            MBeanServer mBeanServer = EasyMock.createNiceMock(MBeanServer.class);
+            DelegatingJmxAuthenticator authenticator = EasyMock.createNiceMock(DelegatingJmxAuthenticator.class);
+            RmiAgentMonitor monitor = EasyMock.createNiceMock(RmiAgentMonitor.class);
 
-        Port port = EasyMock.createMock(Port.class);
-        EasyMock.expect(port.getNumber()).andReturn(PORT).anyTimes();
-        port.bind(Port.TYPE.TCP);
+            Port port = EasyMock.createMock(Port.class);
+            EasyMock.expect(port.getNumber()).andReturn(PORT).anyTimes();
+            port.bind(Port.TYPE.TCP);
 
-        PortAllocator portAllocator = EasyMock.createMock(PortAllocator.class);
-        EasyMock.expect(portAllocator.reserve("JMX", "JMX", PORT)).andReturn(port);
-        portAllocator.release("JMX");
-        EasyMock.expectLastCall();
+            PortAllocator portAllocator = EasyMock.createMock(PortAllocator.class);
+            EasyMock.expect(portAllocator.reserve("JMX", "JMX", PORT)).andReturn(port);
+            portAllocator.release("JMX");
+            EasyMock.expectLastCall();
 
-        EasyMock.replay(mBeanServer, monitor, portAllocator, port);
+            EasyMock.replay(mBeanServer, monitor, portAllocator, port);
 
-        RmiAgent agent = new RmiAgent(mBeanServer, authenticator, portAllocator, monitor);
-        agent.setJmxPort(String.valueOf(PORT));
-        agent.init();
-        agent.destroy();
-        EasyMock.verify(portAllocator, port);
+            RmiAgent agent = new RmiAgent(mBeanServer, authenticator, portAllocator, monitor);
+            agent.setJmxPort(String.valueOf(PORT));
+            agent.init();
+            agent.destroy();
+            EasyMock.verify(portAllocator, port);
+        } catch (ManagementException e) {
+            if (e.getCause() instanceof ExportException) {
+                // ignore
+                System.out.println("*******Skipping RMI Agent test as port is bound");
+            } else {
+                throw e;
+            }
+
+        }
     }
 
     public void testPortRange() throws Exception {
-        MBeanServer mBeanServer = EasyMock.createNiceMock(MBeanServer.class);
-        DelegatingJmxAuthenticator authenticator = EasyMock.createNiceMock(DelegatingJmxAuthenticator.class);
-        RmiAgentMonitor monitor = EasyMock.createNiceMock(RmiAgentMonitor.class);
+        try {
+            MBeanServer mBeanServer = EasyMock.createNiceMock(MBeanServer.class);
+            DelegatingJmxAuthenticator authenticator = EasyMock.createNiceMock(DelegatingJmxAuthenticator.class);
+            RmiAgentMonitor monitor = EasyMock.createNiceMock(RmiAgentMonitor.class);
 
-        Port port = EasyMock.createMock(Port.class);
-        EasyMock.expect(port.getNumber()).andReturn(PORT).anyTimes();
-        port.bind(Port.TYPE.TCP);
+            Port port = EasyMock.createMock(Port.class);
+            EasyMock.expect(port.getNumber()).andReturn(PORT).anyTimes();
+            port.bind(Port.TYPE.TCP);
 
-        PortAllocator portAllocator = EasyMock.createMock(PortAllocator.class);
-        EasyMock.expect(portAllocator.isPoolEnabled()).andReturn(true);
-        EasyMock.expect(portAllocator.allocate("JMX", "JMX")).andReturn(port);
-        portAllocator.release("JMX");
-        EasyMock.expectLastCall();
+            PortAllocator portAllocator = EasyMock.createMock(PortAllocator.class);
+            EasyMock.expect(portAllocator.isPoolEnabled()).andReturn(true);
+            EasyMock.expect(portAllocator.allocate("JMX", "JMX")).andReturn(port);
+            portAllocator.release("JMX");
+            EasyMock.expectLastCall();
 
-        EasyMock.replay(mBeanServer, monitor, portAllocator, port);
+            EasyMock.replay(mBeanServer, monitor, portAllocator, port);
 
-        RmiAgent agent = new RmiAgent(mBeanServer, authenticator, portAllocator, monitor);
-        agent.init();
-        agent.destroy();
-        EasyMock.verify(portAllocator, port);
+            RmiAgent agent = new RmiAgent(mBeanServer, authenticator, portAllocator, monitor);
+            agent.init();
+            agent.destroy();
+            EasyMock.verify(portAllocator, port);
+        } catch (ManagementException e) {
+            if (e.getCause() instanceof ExportException) {
+                // ignore
+                System.out.println("*******Skipping RMI Agent test as port is bound");
+            } else {
+                throw e;
+            }
+
+        }
     }
 
 }
