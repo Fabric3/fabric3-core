3b771db4b6e7f7aa007c03366cc68c548fb56f9c
jmarino <jim.marino@gmail.com>
2013-10-31 15:59:54 +0100

    javadoc

--- extension/binding/fabric3-binding-zeromq-api/src/main/java/org/fabric3/api/binding/zeromq/annotation/ZeroMQ.java
+++ extension/binding/fabric3-binding-zeromq-api/src/main/java/org/fabric3/api/binding/zeromq/annotation/ZeroMQ.java
@@ -62,22 +62,67 @@ public @interface ZeroMQ {
      */
     public Class<?> service() default Void.class;
 
+    /**
+     * Optionally specifies the URI of the target service if it is provided by a component hosted in the current doman.
+     *
+     * @return the target URI
+     */
     public String target() default "";
 
+    /**
+     * Optionally specifies a space-delimited list of [host:port] pairs for target services.
+     *
+     * @return a space-delimited list of [host:port] pairs
+     */
     public String addresses() default "";
 
+    /**
+     * Specifies the socket timeout in microseconds.
+     *
+     * @return the socket timeout in microseconds
+     */
     public long timeout() default 10;
 
+    /**
+     * Specifies the high water mark.
+     *
+     * @return the high water mark
+     */
     public long highWater() default -1;
 
+    /**
+     * Specifies the multicast rate.
+     *
+     * @return the multicast rate
+     */
     public long multicastRate() default -1;
 
+    /**
+     * Specifies the multicast recovery rate.
+     *
+     * @return the multicast recovery rate
+     */
     public long multicastRecovery() default -1;
 
+    /**
+     * Specifies the send buffer size.
+     *
+     * @return the send buffer size
+     */
     public long sendBuffer() default -1;
 
+    /**
+     * Specifies the receive buffer size.
+     *
+     * @return the receive buffer size
+     */
     public long receiveBuffer() default -1;
 
+    /**
+     * Specifies the wire format for message content.
+     *
+     * @return the wire format for message content
+     */
     public String wireFormat() default "";
 
 }
