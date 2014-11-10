65e29ba81c7a37f9e03c0c0d000657352f15babc
jmarino <jim.marino@gmail.com>
2014-11-10 22:26:13 +0100

    update license headers

--- kernel/api/fabric3-host-api/src/main/java/org/fabric3/api/host/classloader/MaskingClassLoader.java
+++ kernel/api/fabric3-host-api/src/main/java/org/fabric3/api/host/classloader/MaskingClassLoader.java
@@ -1,40 +1,19 @@
 /*
  * Fabric3
- * Copyright (c) 2009-2013 Metaform Systems
+ * Copyright (c) 2009-2015 Metaform Systems
  *
- * Fabric3 is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as
- * published by the Free Software Foundation, either version 3 of
- * the License, or (at your option) any later version, with the
- * following exception:
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
  *
- * Linking this software statically or dynamically with other
- * modules is making a combined work based on this software.
- * Thus, the terms and conditions of the GNU General Public
- * License cover the whole combination.
+ *      http://www.apache.org/licenses/LICENSE-2.0
  *
- * As a special exception, the copyright holders of this software
- * give you permission to link this software with independent
- * modules to produce an executable, regardless of the license
- * terms of these independent modules, and to copy and distribute
- * the resulting executable under terms of your choice, provided
- * that you also meet, for each linked independent module, the
- * terms and conditions of the license of that module. An
- * independent module is a module which is not derived from or
- * based on this software. If you modify this software, you may
- * extend this exception to your version of the software, but
- * you are not obligated to do so. If you do not wish to do so,
- * delete this exception statement from your version.
- *
- * Fabric3 is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty
- * of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
- * See the GNU General Public License for more details.
- *
- * You should have received a copy of the
- * GNU General Public License along with Fabric3.
- * If not, see <http://www.gnu.org/licenses/>.
-*/
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.fabric3.api.host.classloader;
 
 import java.io.IOException;
